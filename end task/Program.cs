e35b24d7f8f4ce81c31ec616988c8233babf6807
yanepumba <adress>
2023-12-03 16:10:16 +0300

    Add C# program

---
+++ end task/Program.cs
@@ -0,0 +1,43 @@
+﻿// Ввод массива строк
+Console.WriteLine("Введите строки через запятую:");
+string[] inputArray = Console.ReadLine().Split(',');
+
+// Фильтрация массива
+string[] resultArray = FilterArray(inputArray);
+
+// Вывод результата
+Console.WriteLine("Результат:");
+foreach (var item in resultArray)
+{
+    Console.Write($"{item} ");
+}
+    
+
+    static string[] FilterArray(string[] inputArray)
+{
+    int count = 0;
+    // Подсчет строк, подходящих под условие
+    foreach (var item in inputArray)
+    {
+        if (item.Length <= 3)
+        {
+            count++;
+        }
+    }
+
+    // Создание нового массива
+    string[] resultArray = new string[count];
+    int index = 0;
+
+    // Заполнение нового массива
+    foreach (var item in inputArray)
+    {
+        if (item.Length <= 3)
+        {
+            resultArray[index] = item;
+            index++;
+        }
+    }
+
+    return resultArray;
+}
